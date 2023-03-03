207f2a118bf1c6476bbbdd07d4dd70653ab76135
amnlet <misteriodrey@gmail.com>
2023-03-03 12:00:13 -0300

    calculo feito

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Python 3.11.2](https://img.shields.io/badge/python-3.11.2-blue.svg)](https://www.python.org/downloads/release/python-3112/)
 
-Este é um projeto para testes de calculo matemático e uso de typinghints.
+Este é um projeto para testes de calculo matemático.
 
 
 ## Procedimentos para execução
--- calculo/app.py
+++ calculo/app.py
@@ -1,2 +1,17 @@
-class Bhaskara():
-    pass
+import math
+
+def calc(a,b,c):
+    delta = b*b-4*a*c
+    deltaint = int(delta)
+    raizDelta = math.sqrt(deltaint)
+    baskhara1 = -b+int(raizDelta)/2*a
+    baskhara2 = -b-int(raizDelta)/2*a
+    if deltaint < 0:
+        print("NÃO EXISTEM RAIZES PARA ESSA FUNÇÃO")
+    elif deltaint == float:
+        deltaint = int(deltaint)
+    else:
+        return print(f"Delta: {deltaint}\nRaiz de Delta: {int(raizDelta)}\nResultado: X¹= {baskhara1} X²= {baskhara2}")
+
+
+print(calc(1,2,-24))
